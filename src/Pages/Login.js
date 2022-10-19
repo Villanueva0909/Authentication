a50291606826c1916979cba1e6a2a793cbf9959d
Raul Villanueva <villanuevaraul992@gmail.com>
2022-10-19 18:01:03 +0800

    New navbar

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,12 +1,15 @@
-import { Box, Heading, HStack, Text, VStack } from "@chakra-ui/layout";
-import { Checkbox, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { Box, Heading, HStack, VStack } from "@chakra-ui/layout";
+import { Checkbox, FormControl, FormLabel, Input, Button, } from "@chakra-ui/react";
+import { Navbar } from "../Components/Navbar"
 
 export function Login() {
   return (
+    <>
+    <Navbar />
     <Box
       w={["full", "md"]}
       p={[8, 10]}
-      mt={[20, "vh"]}
+      mt={[15, "vh"]}
       mx="auto"
       border={["none", "1px"]}
       borderColor={["", "gray.300"]}
@@ -16,30 +19,44 @@ export function Login() {
     >
       <VStack spacing={4} align="flex-start" w="full">
         <VStack spacing={1} align={["flex-start", "center"]} w="full">
-          <Heading color="#ED8936">FullTankDevs</Heading>
+          <Heading color="red">Just Lend</Heading>
         </VStack>
-      
-      <FormControl>
-        <FormLabel>E-mail Address</FormLabel>
-        <Input rounded="lg" variant="filled" type="password"></Input>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input rounded="lg" variant="filled" type="password"></Input>
-      </FormControl>
-      <HStack w="full" justify="space-between">
-        <Checkbox>Remember Me</Checkbox>
-        <Button variant="link" colorScheme="orange">
-          Forgot Password?
-        </Button>
-      </HStack>
-        </VStack>
-        <Button mt={['5']} rounded='lg' colorScheme="orange" w={['full']} alignSelf='center'>
-          LOGIN
-        </Button>
-        <Button mt={['5']} rounded='lg' colorScheme="orange" w={['full']} alignSelf='center' variant='outline'>
-          SIGNUP
-        </Button>
+
+        <FormControl>
+          <FormLabel>E-mail Address</FormLabel>
+          <Input rounded="lg" variant="filled" type="password"></Input>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input rounded="lg" variant="filled" type="password"></Input>
+        </FormControl>
+        <HStack w="full" justify="space-between">
+          <Checkbox>Remember Me</Checkbox>
+          <Button variant="link" colorScheme="red">
+            Forgot Password?
+          </Button>
+        </HStack>
+      </VStack>
+      <Button
+        mt={["5"]}
+        rounded="lg"
+        colorScheme="red"
+        w={["full"]}
+        alignSelf="center"
+      >
+        LOGIN
+      </Button>
+      <Button
+        mt={["5"]}
+        rounded="lg"
+        colorScheme="red"
+        w={["full"]}
+        alignSelf="center"
+        variant="outline"
+      >
+        SIGNUP
+      </Button>
     </Box>
+    </>
   );
 }
---
+++ src/Pages/Welcome.js
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <div>
+      <h1>This is Welcome page</h1>
+    </div>
+  );
+}
