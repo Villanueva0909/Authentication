bfc555a46fe77156a1ec452ef5e31f7541f11a01
Raul Villanueva <villanuevaraul992@gmail.com>
2022-10-20 19:52:48 +0800

    Navbar

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,62 +1,70 @@
 import { Box, Heading, HStack, VStack } from "@chakra-ui/layout";
-import { Checkbox, FormControl, FormLabel, Input, Button, } from "@chakra-ui/react";
-import { Navbar } from "../Components/Navbar"
+import {
+  Checkbox,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { Navbar } from '../Components/Navbar';
 
 export function Login() {
   return (
     <>
     <Navbar />
-    <Box
-      w={["full", "md"]}
-      p={[8, 10]}
-      mt={[10, "em"]}
-      mx="auto"
-      border={["none", "2px"]}
-      borderColor={["", "gray.300"]}
-      BorderRadius={20}
-      rounded="lg"
-      boxShadow="dark-lg"
-    >
-      <VStack spacing={4} align="flex-start" w="full">
-        <VStack spacing={1} align={["flex-start", "center"]} w="full">
-          <Heading color="red">Just Lend</Heading>
-        </VStack>
+      <Box
+        w={["full", "md"]}
+        p={[8, 10]}
+        mt={[10, "em"]}
+        mx="auto"
+        border={["none", "2px"]}
+        borderColor={["", "gray.300"]}
+        BorderRadius={20}
+        rounded="lg"
+        boxShadow="dark-lg"
+      >
+        <VStack spacing={4} align="flex-start" w="full">
+          <VStack spacing={1} align={["flex-start", "center"]} w="full">
+            <Heading color="red">Just Lend</Heading>
+          </VStack>
 
-        <FormControl>
-          <FormLabel>E-mail Address</FormLabel>
-          <Input rounded="lg" variant="filled" type="password"></Input>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Password</FormLabel>
-          <Input rounded="lg" variant="filled" type="password"></Input>
-        </FormControl>
-        <HStack w="full" justify="space-between">
-          <Checkbox>Remember Me</Checkbox>
-          <Button variant="link" colorScheme="red">
-            Forgot Password?
-          </Button>
-        </HStack>
-      </VStack>
-      <Button
-        mt={["5"]}
-        rounded="lg"
-        colorScheme="red"
-        w={["full"]}
-        alignSelf="center"
-      >
-        LOGIN
-      </Button>
-      <Button
-        mt={["5"]}
-        rounded="lg"
-        colorScheme="red"
-        w={["full"]}
-        alignSelf="center"
-        variant="outline"
-      >
-        SIGNUP
-      </Button>
-    </Box>
+          <FormControl>
+            <FormLabel>Username</FormLabel>
+            <Input
+              rounded="lg"
+              variant="filled"
+              type="text"
+              id="username"
+              required
+            ></Input>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input
+              rounded="lg"
+              variant="filled"
+              type="password"
+              id="password"
+              required
+            ></Input>
+          </FormControl>
+          <HStack w="full" justify="space-between">
+            <Checkbox>Remember Me</Checkbox>
+            <Button variant="link" colorScheme="red">
+              Forgot Password?
+            </Button>
+          </HStack>
+        </VStack>
+        <Button
+          mt={["5"]}
+          rounded="lg"
+          colorScheme="red"
+          w={["full"]}
+          alignSelf="center"
+        >
+          SIGN IN
+        </Button>
+      </Box>
     </>
   );
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -4,6 +4,8 @@ export function Register() {
   return (
    <>
    <Navbar />
+
+   
    </>
   )
   
